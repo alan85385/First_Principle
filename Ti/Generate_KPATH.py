6faeb3f986d73c1c5dea57bb8b9eaaccee418930
alan85385 <76164946+alan85385@users.noreply.github.com>
2023-07-16 14:05:11 +0800

    Add files via upload

---
+++ Ti/Generate_KPATH.py
@@ -0,0 +1,236 @@
+import sys
+import numpy as np
+
+def main():
+
+    '''
+    The main program flow
+    '''
+
+    library = init() #Initialize with Lattice_Information.txt
+    kpath_matrix, lattice_type, kpath, total_kpoints = ask(library) #Get parameters from user
+    all_kpath, number, N = generate_kpath(kpath_matrix, total_kpoints) #Generate kpoints between kpath's high symmetry points
+    write_file(all_kpath, number, lattice_type, kpath) #Write 2 files: KPOINTS and klabel
+
+def init():
+
+    '''
+    Initialize the lattice types and the k points library through Lattice_Information.txt
+    '''
+
+    file_name = 'Lattice_Information.txt' #Lattice_Information.txt records some lattice types and their k points
+
+    lines = read_file(file_name)
+    library = tranform_lines_into_library(file_name, lines)
+
+    return library
+
+def read_file(file_name):
+
+    '''
+    A function which reads the Lattice_Information.txt file and return lines
+    '''
+    
+    try:
+        with open(file_name, 'r') as fh:
+            lines = fh.readlines()
+
+    except:
+        sys.stderr.write(f'{file_name} is Not Found')
+    
+    return lines
+
+def tranform_lines_into_library(file_name, lines):
+
+    '''
+    A function which transforms lines into the standard form of library and return library
+    '''
+
+    library = {} #collect the information of the lattice types and the k points
+    
+    for line in lines:
+
+        line = line.split()
+
+        if len(line) == 1: #the length of the line = 1 means the line contains the information of the lattice type
+            lattice_type = line[0]
+            library[lattice_type] = {}
+        elif len(line) == 4: #the length of the line = 4 means the line contains the information of the k point
+            high_symmetry_kpoint = line[0]
+            library[lattice_type][high_symmetry_kpoint] = np.array(line[1:4], dtype = 'float')
+        else:
+            print(f'There are some problems in the {file_name} file')
+
+    return library
+
+def ask(library):
+    
+    lattice_type = ask_lattice_type(library)
+    
+    kpath_matrix, kpath = ask_kpath(library, lattice_type)
+    
+    total_kpoints = ask_total_kpoints(kpath_matrix)
+    
+    return kpath_matrix, lattice_type, kpath, total_kpoints
+
+def ask_lattice_type(library):
+
+    '''
+    To ask kpath from user
+    '''
+
+    while True:
+        
+        lattice_type = input(f'Enter the lattice type in the lattice type library {tuple(library.keys())} \n')
+        
+        if lattice_type in library.keys():
+            break
+        else:
+            print(f'There is no \'{lattice_type}\' lattice type in the lattice type library {tuple(library.keys())}')
+
+    return lattice_type
+
+def ask_kpath(library, lattice_type):
+
+    '''
+    The function can collect kpath from user
+    '''
+
+    while True:
+        
+        kpath = input(f'Enter the kpath with the string form \n{tuple(library[lattice_type])}\n')
+        kpath_matrix = np.zeros(shape=(len(kpath),3))
+        all_correct = True
+
+        for i, k in enumerate(kpath):
+
+            if k in library[lattice_type]:
+                kpath_matrix[i] = library[lattice_type][k]
+            else:
+                all_correct = False
+                print(f'{k} doesn\'t exist in {tuple(library[lattice_type].keys())}')
+        
+        if all_correct == True:
+            break
+
+    return kpath_matrix, kpath
+
+def ask_total_kpoints(kpath_matrix):
+    
+    kpath_delta = kpath_matrix[1:] - kpath_matrix[0:-1]
+    length = np.linalg.norm(kpath_delta, axis = 1)
+    
+    weight = np.array(np.round(length*100),dtype='int32')
+    gcd = np.gcd.reduce(weight)
+    weight //= gcd
+    recommend = weight.sum()
+    
+    try:
+        total_kpoints = int(input(f'Enter total kpoints (Recommened:{recommend}*N+1 where N is integer)\n'))
+    except:
+        sys.stderr.write('Invalid argumets!\n')
+        
+    return total_kpoints
+
+def generate_kpath(kpath_matrix, total_kpoints):
+
+    kpath_delta = kpath_matrix[1:] - kpath_matrix[0:-1]
+    length = np.linalg.norm(kpath_delta, axis = 1)
+    
+    partition = np.round((total_kpoints-1)*(length/length.sum()))
+    partition = np.array(partition,dtype='int32')
+    N = total_kpoints-1-partition.sum() #The kpoints which are still not arrange
+    I = np.identity(partition.shape[0],dtype='int32')
+    
+    if N>0:
+    
+        for i in range(N):
+        
+            all_partition = partition+I
+        
+            density = all_partition/length
+            den_std = np.std(density,axis=1)
+        
+            partition = all_partition[den_std==min(den_std)][0]
+            
+    elif N<0:
+    
+        for i in range(-N):
+        
+            all_partition = partition-I
+        
+            density = all_partition/length
+            den_std = np.std(density,axis=1)
+        
+            partition = all_partition[den_std==min(den_std)][0]
+            
+    else:
+        pass
+        
+    all_kpath = np.zeros(shape=(total_kpoints,3))
+    
+    number = [0]
+    
+    for ki, kf, p in zip(kpath_matrix[0:-1],kpath_matrix[1:],partition):
+        
+        i = number[-1]
+        
+        all_kpath[i:i+p] = np.linspace(ki,kf,p,endpoint=False)
+        
+        number.append(i + p)
+        
+    all_kpath[-1] = kpath_matrix[-1]
+    
+    return all_kpath, number, N
+
+def write_file(all_kpath, number, lattice_type, kpath):
+
+    filename = input('Enter the filename to save file\n')
+    
+    write_KPOINTS(filename, all_kpath)
+
+    write_klabel(filename, lattice_type, kpath, number)
+
+def write_KPOINTS(filename, all_kpath):
+    
+    with open(f'KPOINTS_{filename}', 'w') as fh:
+        
+        line = f'{filename}'
+        fh.write(line+'\n')
+        line = '2'
+        fh.write(line+'\n')
+        line = 'Line-mode'
+        fh.write(line+'\n')
+        line = 'Cartesian'
+        fh.write(line+'\n')
+        
+        for k in all_kpath:
+            
+            kx   = k[0]
+            line = f'{kx:7f}' + ' '*(10 - len(f'{kx:7f}'))
+            ky   = k[1]
+            line += f'{ky:7f}' + ' '*(10 - len(f'{ky:7f}'))
+            kz   = k[2]
+            line += f'{kz:7f}'
+            
+            fh.write(line+'\n')
+            
+    print(f'Successfully create the KPOINTS_{filename} file!')
+
+def write_klabel(filename, lattice_type, kpath, number):
+
+    with open('klabel', 'w') as fh:
+
+        fh.write(lattice_type+'\n')
+        
+        for k, n in zip(kpath, number):
+            
+            line = str(k) + ' '
+            line += str(n)
+            fh.write(line+'\n')
+
+    print(f'Successfully create the klabel file!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ Ti/plot_Band_Structure.py
@@ -0,0 +1,186 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+def read_energy():
+    
+    try:
+        with open('EIGENVAL','r') as fh:
+            lines = fh.readlines()
+    except:
+        
+        sys.stderr.write('EIGENVAL file Not Found\n')
+        
+        mainfilename = input('Enter main file name of your energy file\n')
+        
+        with open(mainfilename,'r') as fh:
+            lines = fh.readlines()
+    
+    status = 'transform'
+    
+    return lines, status
+
+def transform_bands_to_E_of_k(lines):
+    
+    numbers_of_bands = []
+    bands = []
+    kpoints = []
+    E_of_k = []
+    
+    Ef = float(input('Enter fermi energy\n'))
+    
+    if len(lines[0].split()) == 4:
+        
+        NBANDS = int(lines[5].split()[2])
+        
+        for line in lines[7:]:
+            
+            l = line.split()
+            
+            if len(l) == 3:
+                E_of_k.append(float(l[1]))
+        
+        E_of_k = np.array(E_of_k).reshape((-1,NBANDS)) - Ef
+        
+    else:
+        for line in lines[4:]:
+    
+            line_split = line.split()
+    
+            if len(line_split) == 2:
+                bands[-1].append(float(line_split[1]))
+            else:
+                bands.append([])
+                kpoints.append([float(line_split[0]), float(line_split[1]), float(line_split[2]), int(line_split[3])])
+                numbers_of_bands.append(int(line_split[4]))
+
+        for bands_of_kpoint in bands:
+            E_of_k.append(bands_of_kpoint[:min(numbers_of_bands)])
+
+        E_of_k = (np.array(E_of_k) - Ef)* 13.6056980659
+
+    status = 'plot'
+    return E_of_k, status
+
+def plot_E_of_k(E_of_k):
+    
+    fig, ax = plt.subplots() 
+    
+    xticks, xlabels, start, end = read_klabel()
+    show = np.full((E_of_k.shape[0]), True)
+    
+    for i,(s, e) in enumerate(zip(start,end)):
+        print(s,e)
+        if s == 0:
+            d = 0
+        else:
+            d += s-end[i-1]
+        ax.plot(np.arange(s-d,e+1-d),E_of_k[s:e+1],color='b')
+        
+    ax.set_title(input('Enter the title of the band plot\n'))
+    ax.set_xlabel('Wave Vector')
+    ax.set_ylabel('E - $E_F$ (eV)')
+    
+    ax.set_xlim(0,E_of_k.shape[0]-d-1)
+    
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels)
+    ax.grid()
+    plt.savefig(input('Enter the name of the .png file\n') + '.png', format='png')
+
+def read_klabel():
+    
+    try:
+        
+        with open('klabel', 'r') as fh:
+        
+            lines = fh.readlines()
+        
+    except:
+        
+        sys.stderr.write('klabel file Not Found\n')
+        
+        kpath_file = input('Enter your kpath file name\n')
+    
+        with open(kpath_file, 'r') as fh:
+        
+            lines = fh.readlines()
+    
+    xlabels = []
+    xticks  = []
+    
+    for line in lines[1:]:
+        
+        xlabels.append(line.split()[0])
+        xticks.append(line.split()[-1])
+    
+    xlabels = np.array(xlabels,dtype='U25')
+    
+    xlabels[xlabels=='G'] = '$\Gamma$'
+    
+    xlabels_new = []
+    xticks_new  = []
+    
+    start = [0]
+    end   = []
+    
+    d = 0
+    for i,(k,t) in enumerate(zip(xlabels,xticks)):
+        
+        if i == 0 or i == len(xlabels)-1:
+            xlabels_new.append(k)
+            xticks_new.append(int(t)-d)
+        else:
+            if k == 'd':
+                xlabels_new.append( xlabels[i-1] + '|' + xlabels[i+1] )
+                d += int(xticks[i+1]) - int(xticks[i-1])
+                xticks_new.append(int(xticks[i-1]))
+                start.append(int(xticks[i+1]))
+                end.append(int(xticks[i-1]))
+            elif k != 'd' and xlabels[i-1] != 'd' and xlabels[i+1] != 'd':
+                xlabels_new.append(k)
+                xticks_new.append(int(t)-d)
+            else:
+                pass
+            
+    end.append(int(xticks[-1]))
+            
+    xticks, xlabels = xticks_new.copy(), xlabels_new.copy()
+    
+    return xticks, xlabels, start, end
+
+if __name__ == '__main__':
+
+    status = 'read'
+    
+    while True:
+        if status == 'read':
+            
+            try:
+                lines, status = read_energy()
+            except:
+                sys.stderr.write('Invalid main file name\n')
+            
+        elif status == 'transform':
+            
+            try:
+                E_of_k, status = transform_bands_to_E_of_k(lines)
+            except:
+                sys.stderr.write('Invalid number\n')
+        
+        elif status == 'plot':
+            
+            plot_E_of_k(E_of_k)
+            status = 'rerun_or_exit'
+        
+        elif status == 'rerun_or_exit':
+
+            status = input('Enter rerun or exit\n')
+            
+            if status == 'rerun':
+                status = 'read'
+            elif status == 'exit':
+                break
+            else:
+                status = 'rerun_or_exit'
+                print('Please enter rerun or exit\n')
